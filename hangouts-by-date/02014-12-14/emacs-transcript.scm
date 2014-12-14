d6826e33db492bcd74fff8d041e1bda4524ff28a
William E. Byrd <webyrd@gmail.com>
2014-12-14 14:55:29 -0700

    editing new transcript

--- hangouts-by-date/02014-12-14/emacs-transcript.scm
+++ hangouts-by-date/02014-12-14/emacs-transcript.scm
@@ -1,3 +1,10 @@
+
+
+
+
+
+
+
 ;; miniKanren Hangout #5: extending the Relational Scheme interpreter
 (load "../../mk-implementations/scheme/mk.scm")
 (load "../../mk-implementations/scheme/pmatch.scm")
@@ -30,11 +37,24 @@
        (lookup x envˆ)])))
 |#
 
+
+(define eval-exp*
+  (lambda (expr* env)
+    (cond
+      [(null? expr*) '()]
+      [else (cons
+              (eval-exp (car expr*) env)
+              (eval-exp* (cdr expr*) env))])))
+
 (define eval-exp
   (lambda (expr env)
     (pmatch expr
       [,x (guard (symbol? x)) ;; variable
        (lookup x env)]
+      [(quote ,datum) datum]
+      [(list . ,expr*)
+       ;; (map (lambda (expr) (eval-exp expr env)) expr*)
+       (eval-exp* expr* env)]
       [(lambda (,x) ,body) ;; abstraction
        `(closure ,x ,body ,env)]
       [(,e1 ,e2) ;; application
@@ -44,12 +64,16 @@
        (let ((proc (eval-exp e1 env))
              (val (eval-exp e2 env)))
          (pmatch proc
-           [(closure ,x ,body ,env)
+           [(closure ,x ,body ,envˆ)
             ;; evaluate body in an extended environment
             ;; in which the environment of the closure is extended
             ;; with a binding between x and val
-            (eval-exp body `((,x . ,val) . ,env))]
+            (eval-exp body `((,x . ,val) . ,envˆ))]
            [,else (error 'eval-exp "e1 does not evaluate to a procedure")]))])))
+
+(eval-exp '(quote 5) '())
+
+(eval-exp '(list ((lambda (x) x) (quote 5)) (quote 6) ((lambda (y) (lambda (z) y)) (quote 7))) '())
 
 
 
@@ -63,6 +87,8 @@
   (lambda (x env out)
     (fresh (y val envˆ)
       (== `((,y . ,val) . ,envˆ) env)
+      (symbolo x)
+      (symbolo y)
       (conde
         [(== x y) (== val out)]
         [(=/= x y) (lookupo x envˆ out)]))))
@@ -75,19 +101,96 @@
 (run* (q) (lookupo q '((x . 5) (y . 6)) '5))
 ;; => (x)
 
+
+
+
+(define unboundo
+  (lambda (x env)
+    (fresh ()
+      (symbolo x)
+      (conde
+        [(== '() env)]
+        [(fresh (y v env^)
+           (== `((,y . ,v) . ,env^) env)
+           (=/= x y)
+           (unboundo x env^))]))))
+
 (define eval-expo
   (lambda (expr env out)
+    (fresh ()
+      (conde
+        [(symbolo expr) ;; variable
+         (lookupo expr env out)]
+        [(== `(quote ,out) expr)
+         (absento 'closure out)
+         (unboundo 'quote env)]
+        [(fresh (expr*)
+           (== `(list . ,expr*) expr)
+           (eval-exp*o expr* env out)
+           (unboundo 'list env))]
+        [(fresh (x body) ;; abstraction
+           (== `(lambda (,x) ,body) expr)
+           (== `(closure ,x ,body ,env) out)
+           (symbolo x)
+           (unboundo 'lambda env))]
+        [(fresh (e1 e2 val x body envˆ) ;; application
+           (== `(,e1 ,e2) expr)
+           (eval-expo e1 env `(closure ,x ,body ,envˆ))
+           (eval-expo e2 env val)
+           (eval-expo body `((,x . ,val) . ,envˆ) out))]))))
+
+(define eval-exp*o
+  (lambda (expr* env out)
     (conde
-      [(symbolo expr) ;; variable
-       (lookupo expr env out)]
-      [(fresh (x body) ;; abstraction
-         (== `(lambda (,x) ,body) expr)
-         (== `(closure ,x ,body ,env) out))]
-      [(fresh (e1 e2 val x body envˆ) ;; application
-         (== `(,e1 ,e2) expr)
-         (eval-expo e1 env `(closure ,x ,body ,envˆ))
-         (eval-expo e2 env val)
-         (eval-expo body `((,x . ,val) . ,envˆ) out))])))
+      [(== '() expr*) (== '() out)]
+      [(fresh (a d res-a res-d)
+         (== (cons a d) expr*)
+         (== (cons res-a res-d) out)
+         (eval-expo a env res-a)
+         (eval-exp*o d env res-d))])))
+
+
+(run* (q) (eval-expo '(list ((lambda (x) x) (quote 5)) (quote 6) ((lambda (y) (lambda (z) y)) (quote 7))) '() q))
+
+(run 1 (q) (eval-expo q '() '(5 6 (closure z y ((y . 7))))))
+
+
+(run* (q) (eval-expo '((lambda (x) x) (lambda (y) y)) '() q))
+
+(run* (q) (eval-expo '((lambda (closure) closure) (lambda (y) y)) '() q))
+
+(run* (q) (eval-expo '((lambda (quote) quote) (lambda (y) y)) '() q))
+
+
+(run* (q) (eval-expo '((lambda (x) (x x)) (lambda (y) y)) '() q))
+
+(run* (q) (eval-expo '((lambda (quote) (quote quote)) (lambda (y) y)) '() q))
+
+
+(run* (q) (eval-expo '(lambda (x) x) '() q))
+;; => ((closure x x ()))
+
+(run* (q) (eval-expo '((lambda (y) y)
+                       (lambda (x) x))
+                     '()
+                     q))
+;; => ((closure x x ()))
+
+(run 2 (q) (eval-expo q '() '(closure x x ())))
+
+
+
+(run* (q) (eval-expo '((lambda (y)
+                         (lambda (x) x))
+                       (quote 5))
+                     '()
+                     q))
+;; => ((closure x x ((y . 5))))
+
+(run 1 (q) (eval-expo q '() '(5 6 (closure z y ((y . 7))))))
+
+(run* (q) (eval-expo '(lambda (5) (lambda (y) y)) '() q)) ;; shows why we need a symbolo in the lambda clause
+;; => ()
 
 (run* (q) (eval-expo '(lambda (x) x) '() q))
 ;; => ((closure x x ()))
@@ -101,19 +204,5 @@
 (run* (q) (eval-expo '(((lambda (x) (lambda (x) x)) (lambda (y) y)) (lambda (w) w)) '() q))
 ;; => ((closure w w ()))
 
-#|
-(define eval-exp
-  (lambda (expr env)
-    (pmatch expr
-      [,x (guard (symbol? x)) ;; variable
-       (lookup x env)]
-      [(lambda (,x) ,body) ;; abstraction
-       `(closure ,x ,body ,env)]
-      [(,e1 ,e2) ;; application
-       (let ((proc (eval-exp e1 env))
-             (val (eval-exp e2 env)))
-         (pmatch proc
-           [(closure ,x ,body ,envˆ)
-            (eval-exp body `((,x . ,val) . ,envˆ))]
-           [,else (error 'eval-exp "e1 does not evaluate to a procedure")]))])))
-|#
+(run* (q) (eval-expo '(quote 5) '() q))
+;; => (5)
